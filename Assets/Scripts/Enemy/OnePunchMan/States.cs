f4baff622ce2c1de44aa61aef18f355326a97d57
Yash Sadhukhan <yskhan61@gmail.com>
2024-05-05 21:00:16 +0530

    Create Chasing State for Patrol Man
    Update OnePunchManStates to States & add Patrolling and Chasing states in that enum.
    Use the base class EnemyController to represent an enemy in IState instead of OnePunchManController.
    Create an interface IStateMachine and make all state machines implement this interface.
    Refactor all your state classes and state machines by updating references of IStateMachine and EnemyController.
    
    Create the following new states:
    Patrolling State
    Chasing State

---
+++ Assets/Scripts/Enemy/OnePunchMan/States.cs
@@ -0,0 +1,11 @@
+﻿namespace StatePattern.Enemy
+{
+    public enum States
+    {
+        IDLE,
+        ROTATING,
+        SHOOTING,
+        PATROLLING,
+        CHASING
+    }
+}
---
+++ Assets/Scripts/Enemy/OnePunchMan/States/PatrollingState.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace StatePattern.Enemy
+{
+    public class PatrollingState : IState
+    {
+        public OnePunchManController Owner { get; set; }
+        private OnePunchManStateMachine stateMachine;
+
+        private int currentPatrollingIndex = -1;
+        private Vector3 destination;
+
+        public PatrollingState(OnePunchManStateMachine stateMachine) => this.stateMachine = stateMachine;
+
+        public void OnStateEnter() 
+        {
+            SetNextWaypointIndex();
+            destination = GetDestination();
+            MoveTowardsDestination();
+        }
+
+        public void Update() 
+        {
+            if (ReachedDestination())
+                stateMachine.ChangeState(States.IDLE);
+        }
+
+        public void OnStateExit() { }
+
+        private void SetNextWaypointIndex()
+        {
+            if (currentPatrollingIndex == Owner.Data.PatrollingPoints.Count - 1)
+                currentPatrollingIndex = 0;
+            else
+                currentPatrollingIndex++;
+        }
+
+        private Vector3 GetDestination() => Owner.Data.PatrollingPoints[currentPatrollingIndex];
+
+        private void MoveTowardsDestination()
+        {
+            Owner.Agent.isStopped = false;
+            Owner.Agent.SetDestination(destination);
+        }
+
+        private bool ReachedDestination() => Owner.Agent.remainingDistance <= Owner.Agent.stoppingDistance;
+    }
+}
